537a4fe948fb9f52e0a97fc697ff3c826f92ef15
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-08-28 16:02:40 +0600

    apply jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,24 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' });
+    }
+    // console.log('Decoded token', decoded);
+    req.decoded = decoded;
+    next();
+  })
+  // console.log('Inside JWT verify', authHeader);
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9huupb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,6 +37,7 @@ async function run() {
   try {
     await client.connect();
     const foodCollection = client.db("foodStock").collection("food");
+    const messageCollection = client.db("userMessages").collection("message");
 
     // Adding or Creating ...
     app.post('/food', async (req, res) => {
@@ -28,7 +48,7 @@ async function run() {
 
     // Find Multiple Document (pagination)...
     app.get('/food', async (req, res) => {
-      const {email} = req.query;
+      const { email } = req.query;
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
       let query;
@@ -86,6 +106,39 @@ async function run() {
       // const count = await cursor.countDocuments();
       const count = await cursor.count();
       res.send({ count })
+    })
+
+
+    // AUTH / jwt ...
+    app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      res.send({ accessToken });
+    })
+
+
+    // User Feedback DATA ...
+    app.post('/feedback-message', async (req, res) => {
+      const order = req.body;
+      const result = await messageCollection.insertOne(order);
+      res.send(result);
+    })
+
+    // Secrate API using token...
+    app.get('/feedback-message', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = messageCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      }
+      else {
+        res.status(403).send({ message: 'forbidden access' })
+      }
     })
 
 
